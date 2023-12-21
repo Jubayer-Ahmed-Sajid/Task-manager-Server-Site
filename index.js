196dbe8d952e5b7efd86e53bc853b713aca588fa
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2023-12-21 21:38:57 +0600

    api for adding new task created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 const port = process.env.PORT || 5000
 require('dotenv').config()
@@ -8,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vqva6ft.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,13 +23,17 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
         // Send a ping to confirm a successful connection
+        const todoCollection = client.db('TaskDB').collection('todoList')
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        app.post('/todo',async(req,res)=>{
+            const task = req.body
+            const result = await todoCollection.insertOne(task)
+            res.send(result)
+        })
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
     }
 }
 run().catch(console.dir);
